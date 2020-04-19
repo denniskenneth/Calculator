d9a9537a15c4daa3f9ff1e8eef06d5612da4a26c
kenneth dennis ifeanyichukwu <dennyify123@gmail.com>
2020-04-19 14:54:54 +0000

    calculator created with just basic CSS and no useful JS

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-let num1;
-let num2;
-const addKey =  document.querySelector('#add-key');
+let buttonValue = document.querySelectorAll(".button");
 
-function addition(array){
-    return array.reduce((num1, num2) => num1 + num2, 0);
+//INSERT FUNCTION
+function insert(num){
+    document.getElementsByClassName("textview").value += num;
 }
 
-function substraction (array){
-    return array.reduce((num1, num2) => num1 - num2, 0);
-}
-
-function multiplication (array){
-    return array.reduce((num1, num2) => num1 * num2, 0);
-}
-
-function division (array){
-    return array.reduce((num1, num2) => num1/num2, 0);
-}
-
-function operate (operator) {
-    
-}
