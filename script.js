9fb0817870657702413872676447114a0c80f58d
kenneth dennis ifeanyichukwu <dennyify123@gmail.com>
2020-04-27 06:22:10 +0000

    final commit simple calculator finished

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-let buttonValue = document.querySelectorAll(".button");
+let buttons = document.getElementsByClassName('button');
+let numButtons = document.getElementsByClassName('num-btn');
+let opeButtons = document.getElementsByClassName('ope-btn');
+let clearButtons = document.getElementById('clear-btn');
+let equalButtons = document.getElementById('equ-btn');
+let screen = document.querySelector('.screen');
 
-//INSERT FUNCTION
-function insert(num){
-    document.getElementsByClassName("textview").value += num;
-}
+ for (let i=0; i<numButtons.length; i++) {
+     numButtons[i].addEventListener('click', function() {
+        screen.value += this.value
+         console.log(this.value);
+     });
+ }
 
+ for (let j=0; j < opeButtons.length; j++) {
+    opeButtons[j].addEventListener('click', function() {
+        screen.value += this.value
+    });
+ }
+
+ equalButtons.addEventListener('click', function(){
+     let result = eval(screen.value);
+     screen.value = result;
+ })
+
+clearButtons.addEventListener('click', function() {
+    screen.value = ''
+})
